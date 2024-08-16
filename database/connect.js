2f482e6eac367c3d537fd90724486d8a61f1d0a1
THXIIIRTEEN <danilmatsarov@yandex.ru>
2024-08-16 15:04:47 +0500

    Updated mongo db url

--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URL = 'mongodb://localhost:27013/triiiple';
+const DB_URL = 'mongodb://localhost:27017/triiiple';
 
 async function connectDatabase() {
     try {
